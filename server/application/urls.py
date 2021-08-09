aab089235516cb4f1e6e18358a79f385b2818d85
Tahmid1406 <tahmidpranto1406@gmail.com>
2021-08-09 21:55:04 +0600

    auth based endpoint restriction added

--- server/application/urls.py
+++ server/application/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('account', account, name='account'),
     path('login', auth_views.LoginView.as_view(
         template_name='application/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout', auth_views.LogoutView.as_view(
+        template_name='application/logout.html'), name='logout'),
 ]
